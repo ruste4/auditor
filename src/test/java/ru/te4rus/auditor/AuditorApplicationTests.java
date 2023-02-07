5374a8671c3cec9cc020f62a740bfe34c369cab0
ruste4 <te4rus@yandex.ru>
2023-02-07 16:47:55 +0500

    first commit

---
+++ src/test/java/ru/te4rus/auditor/AuditorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.te4rus.auditor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
